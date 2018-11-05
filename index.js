1138ccd28f6b95355a4c01f7a996d01a318b5475
SATYA JUTTIGA <satyatanmayee@gmail.com>
2018-11-05 12:12:26 +0530

    public

--- index.js
+++ index.js
@@ -3,3 +3,7 @@ const express=require('express');
 const app=express();
 
 app.use(express.static(__dirname + '/public'));
+
+const port=3000;
+
+app.listen(port,() => console.log(`server started on port ${port}`));
