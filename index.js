e58d027235dadc02adaed98cbe043924390473d4
SATYA JUTTIGA <satyatanmayee@gmail.com>
2018-11-05 12:24:30 +0530

    multer

--- index.js
+++ index.js
@@ -1,5 +1,12 @@
 const express=require('express');
-
+const bodyParser = require('body-parser');
+const path = require('path');
+const crypto = require('crypto');
+const mongoose = require('mongoose');
+const multer = require('multer');
+const GridFsStorage = require('multer-gridfs-storage');
+const Grid = require('gridfs-stream');
+const methodOverride = require('method-override');
 const app=express();
 
 app.use(express.static(__dirname + '/public'));
