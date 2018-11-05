0568cef93b87cfdc0c44f8e76d176c8930efd642
SATYA JUTTIGA <satyatanmayee@gmail.com>
2018-11-05 13:50:10 +0530

    gridfs

--- index.js
+++ index.js
@@ -7,10 +7,18 @@ const multer = require('multer');
 const GridFsStorage = require('multer-gridfs-storage');
 const Grid = require('gridfs-stream');
 const methodOverride = require('method-override');
+const env=require('./config/env');
+
 const app=express();
 
+app.use(bodyParser.json());
+app.use(methodOverride('_method'));
 app.use(express.static(__dirname + '/public'));
 
 const port=3000;
+
+mongoose.connect(env.mongodb.dbURI,() => {
+    console.log('connected to mongo db');
+});
 
 app.listen(port,() => console.log(`server started on port ${port}`));
